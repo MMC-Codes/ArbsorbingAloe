f9cd0a88cd0debfd4dc698abd2afa69b7b2e7fe3
XMBomb <11milan@gmx.ch>
2015-05-30 18:01:04 +0200

    Basic Project Setup

---
+++ src/main/java/mmc/codes/absorbing_aloe/App.java
@@ -0,0 +1,13 @@
+package mmc.codes.absorbing_aloe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
